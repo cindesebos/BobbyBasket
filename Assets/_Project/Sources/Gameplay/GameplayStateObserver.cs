115687fa8b67c82e2a1161f61eb79172bf1250dc
Dinooyx <y07programmer@gmail.com>
2024-10-15 22:31:32 +0200

    Init function menu

--- Assets/_Project/Sources/Gameplay/GameplayStateObserver.cs
+++ Assets/_Project/Sources/Gameplay/GameplayStateObserver.cs
@@ -11,14 +11,17 @@ namespace Sources.Gameplay
         private readonly CharacterInput _characterInput;
         private readonly ItemsSpawner _itemsSpawner;
         private readonly ItemsPool _itemsPool;
+        private readonly CoinCounter _coinCounter;
 
         [Inject]
-        private GameplayStateObserver(GameOverPopUp gameOverPopUp, CharacterInput characterInput, ItemsSpawner itemsSpawner, ItemsPool itemsPool)
+        private GameplayStateObserver(GameOverPopUp gameOverPopUp, CharacterInput characterInput, 
+        ItemsSpawner itemsSpawner, ItemsPool itemsPool, CoinCounter coinCounter)
         {
             _gameOverPopUp = gameOverPopUp;
             _characterInput = characterInput;
             _itemsSpawner = itemsSpawner;
             _itemsPool = itemsPool;
+            _coinCounter = coinCounter;
         }
 
         public void OnGameOver()
@@ -26,7 +29,7 @@ namespace Sources.Gameplay
             _itemsPool.HideAllItems();
             _itemsSpawner.StopWork();
             _characterInput.Disable();
-            _gameOverPopUp.Show();
+            _gameOverPopUp.Show(_coinCounter.Score);
         }
     }
 }
--- Assets/_Project/Sources/UI/GameOverPopUp.cs
+++ Assets/_Project/Sources/UI/GameOverPopUp.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 namespace Sources.UI
 {
     public class GameOverPopUp : MonoBehaviour
     {
-        [SerializeField] private GameObject _objectToShow;
+        [SerializeField] private float _showEffectDuration;
+        [Space]
 
-        public void Show()
+        [SerializeField] private GameObject _objectToShow;
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private TextMeshProUGUI _scoreText;
+
+        private void Start()
         {
+            _canvasGroup.alpha = 0f;
+            _objectToShow.SetActive(false);
+        }
+
+        public void Show(int score)
+        {
+            _scoreText.text += score.ToString();
             _objectToShow.SetActive(true);
+            _canvasGroup.DOFade(1f, _showEffectDuration).SetEase(Ease.InOutQuad);
         }
     }
 }
